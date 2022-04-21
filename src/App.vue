22fe9b8030a3d343e7982218f9e5e89f93950638
Robin9800 <robin.costanzo@gmail.com>
2022-04-21 17:36:51 +0200

    Old repository added

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HeaderComponent/>
+    <MainComponent/>
+</div>
+ 
+</template>
+
+<script>
+import MainComponent from './components/MainComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <h1>BOOLFLIX</h1>
+    <!--Associamo all'input, attraverso la direttiva Vue "v-model", textTosearch-->
+    <input v-model="textToSearch" type="text" placeholder="Ricerca per titolo">
+    <button>Cerca!</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    data(){
+      return{
+        /*Inizializzaimo la costante, che rappresenterà ciò che l'user scriverà nella
+        barrad di ricerca, a stringa vuota*/
+        textToSearch:''
+      }
+    }
+}
+</script>
+
+<style>
+.header{
+  display: flex;
+  flex-direction: column;
+  background-image: url("../../public/img/background.png");
+  height: 30vh;
+}
+.header h1{
+  font-size: 100px;
+  color: #e50914;
+  align-self: center;
+}
+.header input,button{
+  align-self: center;
+  width: 120px;
+}
+</style>
